01196388a3ee7ce502417e0a19801d61cfce432f
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2022-06-04 23:33:40 -0400

    Base boiler code created

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const portNumber = process.env.portNumber || 3000
+
+const app = express()
+
+// MiddleWare
+const corsOptions = {
+    origin: '*'
+}
+
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// controllers
+
+app.get('*', (req, res) => {
+    res.status(404).json({
+        message: 'Not Found'
+    })
+})
+
+app.listen(portNumber, () => console.log(`listening on por ${portNumber}`))
