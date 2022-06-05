edb31dba0d39632c2f1f63d262d5fd699d6bad5a
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2022-06-05 13:27:33 -0400

    Created expense-category model and migration

--- backend/models/expense_category.js
+++ backend/models/expense_category.js
@@ -3,52 +3,48 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Budget extends Model {
+  class Expense_Category extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ User, Expense_Category, Income }) {
-      this.hasMany(Expense_Category,{
-        foreignKey: 'budget_id',
+    static associate({ Budget, Expense }) {
+      this.hasMany(Expense, {
+        foreignKey: 'expenses_category_id',
         onDelete: 'CASCADE'
       })
-      this.hasMany(Income ,{
+      this.belongsTo(Budget, {
         foreignKey: 'budget_id',
-        onDelete: 'CASCADE'
-      })
-      this.belongsTo(User,{
-        foreignKey: 'user_id',
         onDelete: 'CASCADE'
       })
     }
   }
-  Budget.init({
-    budget_id: {
-      type: DataTypes.UUID,
+  Expense_Category.init({
+    expenses_category_id: {
+      type:  DataTypes.UUID,
       primaryKey: true,
       unique: true,
       defaultValue: DataTypes.UUIDV4
     },
-    user_id: {
+    budget_id: {
       type: DataTypes.UUID,
       allowNull: false
     },
-    name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         noEmpty: true
       }
     },
-    budget_date: {
-      type: DataTypes.DATEONLY,
+    set_amount: {
+      type: DataTypes.INTEGER,
       allowNull: false
     }
   }, {
     sequelize,
-    modelName: 'Budget',
+    modelName: 'Expense_Category',
   });
-  return Budget;
+  return Expense_Category;
 };
