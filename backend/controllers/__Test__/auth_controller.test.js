30e1dd9ab1d5475b0d81a34650ec71a52b24344d
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2022-06-12 13:02:41 -0400

    Auth Controllers Unit Test Pass

--- backend/controllers/__Test__/auth_controller.test.js
+++ backend/controllers/__Test__/auth_controller.test.js
@@ -2,16 +2,27 @@ const app = require('../../app')
 const request = require('supertest')
 
 describe('Auth Controller Tests', () => {
-    it('returns 200 and json when posting to signup', async ()=>{
-        const response = await request(app)
+    it('Signup-test: returns 200 and json when posting to signup', async ()=>{
+        const res = await request(app)
         .post('/auth/signup')
         .send({
-            "email" : "lola@lol.com",
-            "password" : "lol123",
-            "name": "Lola",
-            "last_name": "Lolz"
+            email : "lola@lol.com",
+            password : "lol123",
+            name: "Lola",
+            last_name: "Lolz"
         })
-        expect(response.status).toEqual(200)
-        expect(response.body.message).toEqual('Account Created')
+        expect(res.status).toEqual(200)
+        expect(res.body.message).toEqual('Account Created')
+    })
+
+    it('Login-test: returns status 200, and a json token', async ()=>{
+        const res = await request(app)
+        .post('/auth/login')
+        .send({
+            email: "lola@lol.com",
+            password: "lol123",
+        })
+        await expect(res.status).toEqual(200)
+        await expect(res.body).toHaveProperty('token')
     })
 })
